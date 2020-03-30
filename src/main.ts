6938dba33b1af78fe04fa3803b2e98e575c1c0e3
Hirava Anaghan <hirava2701@gmail.com>
2020-03-30 13:33:05 -0400

    Add client built files in nest server, change heroku-postbuild script.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
+import * as express from 'express';
+import {join} from 'path';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix('/api');
+    app.use(express.static(join(__dirname, "client/build")));
     await app.listen(process.env.PORT || 8080);
 }
 
