59acb6887e63ee301e4de9b143857cfaad500585
Hirava Anaghan <hirava2701@gmail.com>
2020-03-30 17:03:52 -0400

    Some fix

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+const path_1 = require("path");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.setGlobalPrefix('/api');
+    app.use('/', express.static(path_1.join(process.cwd(), "/client/build")));
+    console.log(path_1.join(process.cwd(), "/client/build"));
     await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
